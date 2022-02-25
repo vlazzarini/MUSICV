9b3647ce8eb771d0867916c3fe4dcf9c6e8c2de6
Victor Lazzarini <victor.lazzarini@mu.ie>
2022-02-25 15:21:53 +0000

    options to run separate passes

--- src/music5.c
+++ src/music5.c
@@ -33,64 +33,123 @@ extern int pass2_(int *);
 extern int pass3_();
 extern int raw2wav(const char *, const char *, int, int); 
 
-int main(int argc, char *argv[]) {
-  if (argc > 2) {
+typedef struct _params_ {
+  const char *score;
+  const char *fname;
+  int  pass;
+} params;
+
+int argdecode(int argc, const char *argv[], params *p) {
+  for(int i = 1; i < argc; i++) {
+    if(*argv[i] == '-') {
+      if(*(argv[i]+1) == 'p') {
+	if(*(argv[i]+2) == '\0') i++;
+	p->pass = strtol(argv[i],NULL,0);
+      }
+      if(*(argv[i]+1) == 'h') {
+        printf("usage: %s [-p pass] [score] [output.wav] \n", argv[0]);
+        return 0;
+      }
+    }
+    else {
+      if(p->score == NULL) {
+	p->score = argv[i];
+      }
+      else {
+	p->fname = argv[i];
+      }
+    }
+  }
+  return 1;
+}
+
+int main(int argc, const char *argv[]) {
+  params p = { NULL, NULL, 0 };
+  if (argdecode(argc,argv,&p)) {
     FILE *fin,*fout;
     int ret = 0;
     int subr = 0;
     char buffer[BUFSIZE];
-    fin = fopen(argv[1],"r");
-    if(fin) {
-      fout = fopen("score","w");
-      if(fout) do {
-          ret = fread(buffer,1,BUFSIZE,fin);
-          fwrite(buffer,1,ret,fout);
-	} while(ret);
-      else {
+    if(p.pass == 0 || p.pass == 1) {
+      if(p.score) {
+	fin = fopen(p.score,"r");
+	if(fin) {
+	  fout = fopen("score","w");
+	  if(fout) {
+	    do {
+	      ret = fread(buffer,1,BUFSIZE,fin);
+	      fwrite(buffer,1,ret,fout);
+	    } while(ret);
+	    fclose(fout);
+	  }
+	  else {
+	    fclose(fin);
+	    printf("could not open score file for writing\n");
+	    return -1;
+	  }
+	}
+	else {
+	  printf("could not open %s for reading\n", argv[1]);
+	  return -1;
+	}
 	fclose(fin);
-	printf("could not open score file for writing\n");
-        return -1;
-      }
-      rewind(fin);
-      do {
-	ret = fscanf(fin," COMMENT: CONVT %d ; \n", &subr);
-	fscanf(fin, "%s\n",buffer);
-      } while(!feof(fin) || ret == 0);
-      printf(" CONVT: %d \n", subr);
-      fclose(fin);      
-      fclose(fout);
-    } else {
-      printf("could not open %s for reading\n", argv[1]);
-        return -1;
+      } 
+      ret = pass1_();
     }
-    ret = pass1_();
-    if(ret == 0) {
-      ret = pass2_(&subr);
+    if(p.pass == 0 || p.pass == 2) {
+      if(ret == 0){
+	fin = fopen("score","r");
+	if(fin) {
+	  do {
+	    ret = fscanf(fin," COMMENT: CONVT %d ; \n", &subr);
+	    fscanf(fin, "%s\n",buffer);
+	  } while(!feof(fin) || ret == 0);
+	  printf(" CONVT: %d \n", subr);
+	  fclose(fin);      
+	}
+	else {
+	  fclose(fin);
+	  printf("could not open score file for writing\n");
+	  return -1;
+	}
+	ret = pass2_(&subr);
+      }
+      else {
+	printf("pass 1 failed\n");
+	return 1;
+      }
+    }
+    if(p.pass == 0 || p.pass == 3) {
+      if(ret == 0) ret = pass3_();
+      else {
+	printf("pass 2 failed\n");
+	return 2;
+      } 
       if(ret == 0) {
-	ret = pass3_();
-	if(ret == 0) {
-	  int chn = 1;
-	  int sr = 44100;
-	  fin = fopen("snd_params.txt","r");
+	int chn = 1;
+	int sr = 44100;
+	fin = fopen("snd_params.txt","r");
+	if(fin) {
 	  fscanf(fin, "%d", &chn);
 	  fscanf(fin, "%d", &sr);
 	  fclose(fin);
-	  printf(" PASS III completed successfully\n"
-		 "Created snd.raw (32-bit floas, sr = %d KHz, %s)\n", sr,
-		 chn == 2 ? "stereo" : "mono");
-	  ret = raw2wav("snd.raw",argv[2], chn == 2 ? 2 : 1, sr);
-	  if(ret) {
-	    printf("Failed to convert snd.raw to %s\n", argv[2]);
-	    return -1;
-	  }
-	  return 0;
 	}
-	else printf("pass 3 failed\n");
-      } else printf("pass 2 failed\n");
-    } else printf("pass 1 failed\n");
-    return 1;
-  } else printf("usage: %s score output.wav \n", argv[0]);
-  return -1;
+	printf(" PASS III completed successfully\n"
+	       "Created snd.raw (32-bit floas, sr = %d KHz, %s)\n", sr,
+	       chn == 2 ? "stereo" : "mono");
+	ret = raw2wav("snd.raw", p.fname ? p.fname : "out.wav", chn == 2 ? 2 : 1, sr);
+	if(ret) {
+	  printf("Failed to convert snd.raw to %s\n", argv[2]);
+	  return -1;
+	}
+	return 0;
+      } else {
+	printf("pass 3 failed\n");
+	return 3;
+      }
+    }
+    return 0;
+  }  else return -1;
 }
 
 
