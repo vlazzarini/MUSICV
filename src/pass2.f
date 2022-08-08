99b750ede9232b2118fcda034573aa42b00b5db4
nemanja81 <80408612+nemanja81@users.noreply.github.com>
2022-08-08 12:09:44 +0200

    convt20 added, scores modified

--- src/pass2.f
+++ src/pass2.f
@@ -307,7 +307,7 @@ c added back in
       SUBROUTINE CONVT
       COMMON IP(10),P(100),G(1000),I(1000),T(1000),D(10000),IXJQ,TLAST,
      *     BLAST, IROUT
-      GOTO(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19),IROUT
+      GOTO(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20),IROUT
       CALL CONVT0
       GOTO 9999
  1    CALL CONVT1
@@ -348,6 +348,8 @@ c added back in
       GOTO 9999
  19   CALL CONVT19
       GOTO 9999
+ 20   CALL CONVT20
+      GOTO 9999 
  9999 RETURN
       END
 
@@ -778,6 +780,19 @@ C
       GOTO100
  100  RETURN
       END
+
+C     RVN CONVT for Mathews, 1969, p.54
+C
+      SUBROUTINE CONVT20
+      COMMON IP(10),P(100),G(1000)
+      IF(P(1).NE.1.)GOTO100
+      F=511./G(4)
+      P(6)=F*P(6)
+      P(7)=F*P(7)
+      P(8)=F*P(8)
+      P(9)=F*P(9)
+ 100  RETURN
+      END      
 
 C     ERRO1 GENERAL ERROR ROUTINE
 C     *** MUSIC V ***
